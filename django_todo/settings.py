eaad652593027cb46e46a62a7c82a6d65e03d569
MartinaP87 <bleach1987@hotmail.it>
2022-10-27 11:58:36 +0000

    Add an add_item file and dispaly it on the server

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+if os.path.isfile("env.py"):
+    import env
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
